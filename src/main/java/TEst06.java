d9263b16d77fbff97114154eb4e243a0718f7a30
bit <mikekuk2005@naver.com>
2018-10-05 15:13:30 +0900

    test06 renew

--- src/main/java/TEst06.java
+++ src/main/java/TEst06.java
@@ -2,5 +2,6 @@
 public class TEst06 {
     public static void main(String args[]) {
         System.out.println("dddddddddddddddddddd");
+        System.out.println("dddddddddddddddddddd");System.out.println("dddddddddddddddddddd");System.out.println("dddddddddddddddddddd");
     }
 }
