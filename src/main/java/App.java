0e8c3e5872bbf0a855544691a84dedd76cde9a7f
friday1015 <friday1015@naver.com>
2018-10-05 15:48:10 +0900

    masg

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,8 @@
  */
 public class App {
     public String getGreeting() {
-        String msg = "갓종규";
-        return msg;
+        String masg = "김종규";
+        return masg;
     }
 
     public static void main(String[] args) {
