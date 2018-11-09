d9787d7745165e32457143faeb45daf7f8b48512
jhan <duke8612@nate.com>
2018-11-09 11:06:43 +0900

    결제,결제완료,계좌등록 신규 commit 메인페이지 반응형 처리

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,3 +1,22 @@
+/* header */
+
+(function(){
+	$('.ProfilePhoto').on('click',openMenu);
+
+	function openMenu(){
+		/* to do */
+	}
+
+	$('.gnb-menu__container').on('click',openAside);
+
+	function openAside(){
+		console.log($('.Drawer.Drawer--appear'));
+		$('.Drawer.Drawer--appear').css('visibility','visible');
+	}
+})()
+
+
+/* main page */
 $('.map-link').on('mouseenter', function () {
 		
 		var classNm = $(this).attr('class');
