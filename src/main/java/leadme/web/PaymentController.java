8da72e8ccb0cd90288db25306d1301f0c77c1991
jhan <duke8612@nate.com>
2018-11-16 21:22:02 +0900

    결제 작업중

--- src/main/java/leadme/web/PaymentController.java
+++ src/main/java/leadme/web/PaymentController.java
@@ -8,6 +8,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -49,6 +50,12 @@ public class PaymentController {
       */
       return "payment/payment";
     }
+    
+    @PostMapping
+    public String payment() {
+      return "";
+    }
+        
 }
 
 
